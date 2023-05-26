6203e77fa75ec357412a91efdd606e8a52e9738f
nurfauzi12 <mfajarnurfauzi8823@gmail.com>
2023-05-26 15:41:39 +0700

    [R-FE-WEB-09/001]: Features - News Page

---
+++ pages/news/index.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from 'next'
+import { NewsPage } from '@utama/containers/Public'
+
+const News: NextPage = () => <NewsPage />
+
+export default News
--- src/containers/Public/Landing/index.tsx
+++ src/containers/Public/Landing/index.tsx
@@ -26,50 +26,6 @@ import SectionFeature from './components/feature'
 import SectionNews from './components/news'
 
 export default function PageSignIn() {
-  const { Title, Text, Paragraph } = Typography
-
-  const sampleLabels = [
-    'Self Improvement',
-    'Writing',
-    'Productivity',
-    'Relationships',
-    'Politics',
-    'Education',
-  ]
-
-  const features = [
-    {
-      icon: ICBrieftCase,
-      title: 'Easy to Use',
-      desc: 'Working together with your team to make decisions',
-    },
-    {
-      icon: ICTrello,
-      title: 'Easy to Use',
-      desc: 'Working together with your team to make decisions',
-    },
-    {
-      icon: ICSend2,
-      title: 'Easy to Use',
-      desc: 'Working together with your team to make decisions',
-    },
-    {
-      icon: ICGift,
-      title: 'Easy to Use',
-      desc: 'Working together with your team to make decisions',
-    },
-    {
-      icon: ICWifiOff,
-      title: 'Easy to Use',
-      desc: 'Working together with your team to make decisions',
-    },
-    {
-      icon: ICGlobe,
-      title: 'Easy to Use',
-      desc: 'Working together with your team to make decisions',
-    },
-  ]
-
   return (
     <PublicLayout>
       <SectionHeader />
